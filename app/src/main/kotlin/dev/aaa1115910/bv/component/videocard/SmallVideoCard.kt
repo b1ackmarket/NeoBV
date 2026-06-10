baea4be6e419ef2c4162891cf368f0613900341d
b1ackmarket <senluodezhongxuyao@163.com>
2026-06-10 22:07:44 +0800

    fix: 更新网页模式UA

--- app/src/main/kotlin/dev/aaa1115910/bv/component/videocard/SmallVideoCard.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/component/videocard/SmallVideoCard.kt
@@ -231,7 +231,7 @@ private fun FocusPreviewCover(
         val player = ExoPlayerFactory().create(
             context = context,
             options = VideoPlayerOptions(
-                userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 Edg/149.0.0.0",
                 referer = "https://www.bilibili.com",
                 enableFfmpegAudioRenderer = Prefs.enableFfmpegAudioRenderer,
                 enableSoftwareVideoDecoder = Prefs.enableSoftwareVideoDecoder,
